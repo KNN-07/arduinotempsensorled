208dd75b175faae410af66af8c49aacb61178b05
KNN-07 <55886589+KNN-07@users.noreply.github.com>
2021-12-16 17:53:30 +0700

    fix

--- LM35main.ino
+++ LM35main.ino
@@ -1,16 +1,17 @@
 int sensorPin = A0;
 void setup() {
     Serial.begin(9600);
+    pinMode(7, OUTPUT);
 }
 void loop() {
     int read = analogRead(sensorPin);
     float voltage = read * 5.0 / 1024.0;
     float temp = voltage * 100.0; // mV sang C
     Serial.println(temp);
-    if (temp > 30){
-        digitalWrite(13, HIGH);
+    if (temp < 30){
+        digitalWrite(7, HIGH);
     } else {
-        digitalWrite(13, LOW);
+        digitalWrite(7, LOW);
     }
-    delay(1000);
+    delay(2000);
 }
