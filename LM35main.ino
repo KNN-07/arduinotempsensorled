b94b53aaaf92a12beffe14048b5f843bb6b3ff46
KNN-07 <55886589+KNN-07@users.noreply.github.com>
2021-12-16 07:10:36 +0700

    lm35 support

---
+++ LM35main.ino
@@ -0,0 +1,16 @@
+int sensorPin = A0;
+void setup() {
+    Serial.begin(9600);
+}
+void loop() {
+    ​int read = analogRead(sensorPin);
+    float voltage = reading * 5.0 / 1024.0;
+    float temp = voltage * 100.0 // mV sang C
+    ​Serial.println(temp);
+    if (celsius > 30){
+        digitalWrite(13,HIGH);
+    } else {
+        digitalWrite(13, LOW);
+    }
+    delay(1000);
+}
